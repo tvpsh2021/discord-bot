40da31d9b3a0c22bc1566360ee142ce2f93b7c53
Jimmy <tvpsh2020@gmail.com>
2021-02-25 15:36:26 +0800

    feat: add node version and adjust command

--- webhook.js
+++ webhook.js
@@ -9,15 +9,16 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content === 'ping') {
+  if (message.content.startsWith('ping')) {
     message.channel.send('pong');
   }
 
-  if (message.content.includes('search')) {
-    const keywords = message.content.split(' ');
-    const kw = keywords[1];
-    console.log(`Incoming YT search console: ${kw}`);
-    const url = encodeURI(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.GOOGLE_API_TOKEN}&type=video&q=${kw}`);
+  if (message.content.startsWith('yt')) {
+    const parsedContent = message.content.split(' ');
+    parsedContent.shift();
+    const keyword = (parsedContent.join(' ')).trim();
+    console.log(`Incoming YT search console: ${keyword}`);
+    const url = encodeURI(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.GOOGLE_API_TOKEN}&type=video&q=${keyword}`);
     axios.get(url).then(res => {
       const vid = res.data.items[0].id.videoId;
       message.channel.send(`https://www.youtube.com/watch?v=${vid}`);
